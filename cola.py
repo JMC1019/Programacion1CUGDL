6431a9cf6bd15d73bd1b266847a292e5f9b58a5d
JMC1019 <josuenov3@pm.me>
2025-10-09 12:12:09 -0600

    Comentarios

--- cola.py
+++ cola.py
@@ -6,10 +6,12 @@ cola = []
 # --- Funciones básicas ---
 def esta_vacia(cola):
     return len(cola) == 0
+#Verifica que la lista este vacia#
 
 def encolar(cola, elemento):  # Enqueue
     cola.append(elemento)
     print(f"Se encoló: {elemento}")
+#Agrega un elemento al final de la lista#
 
 def desencolar(cola):  # Dequeue
     if not esta_vacia(cola):
@@ -18,6 +20,7 @@ def desencolar(cola):  # Dequeue
         return elemento
     else:
         print("La cola está vacía. No se puede desencolar.")
+#Elimina el primer elemento de la lista#
 
 def ver_frente(cola):  # Front
     if not esta_vacia(cola):
@@ -25,26 +28,33 @@ def ver_frente(cola):  # Front
         return cola[0]
     else:
         print("La cola está vacía.")
+#Muestra el primer elemento de la lista#
 
 def mostrar_cola(cola):
     print("Estado actual de la cola:", cola)
-
+#Muestra todos los elementos de la lista#
 
 # --- Uso paso a paso ---
 print("¿La cola está vacía?", esta_vacia(cola))
+#Verifica que la lista este vacia#
 
 encolar(cola, "Persona 1")
 encolar(cola, "Persona 2")
 encolar(cola, "Persona 3")
+#Agrega 3 elementos a la lista#
 
 mostrar_cola(cola)
 ver_frente(cola)
+#Muestra todo los elementos y el primer elemento de la lista#
 
 desencolar(cola)
 mostrar_cola(cola)
+#Elimina el primer elemento y muestra la lista#
 
 desencolar(cola)
 desencolar(cola)
 desencolar(cola)  # Intento extra
+#Intenta eliminar 3 veces el primer elemento de la lista, el último intento marcara que la lista esta vacia#
 
 print("¿La cola está vacía?", esta_vacia(cola))
+#Verifica que la lista este vacia, devuelve True#
--- pila.py
+++ pila.py
@@ -6,10 +6,12 @@ pila = []
 # --- Funciones básicas ---
 def esta_vacia(pila):
     return len(pila) == 0
+#Verifica que la lista este vacia#
 
 def apilar(pila, elemento):  # Push
     pila.append(elemento)
     print(f"Se apiló: {elemento}")
+#Agrega un elemento a la lista#
 
 def desapilar(pila):  # Pop
     if not esta_vacia(pila):
@@ -18,6 +20,7 @@ def desapilar(pila):  # Pop
         return elemento
     else:
         print("La pila está vacía. No se puede desapilar.")
+#Elimina el ultimo elemento de la lista#
 
 def ver_tope(pila):  # Peek
     if not esta_vacia(pila):
@@ -25,9 +28,11 @@ def ver_tope(pila):  # Peek
         return pila[-1]
     else:
         print("La pila está vacía.")
+#Muestra el ultimo elemento de la lista#
 
 def mostrar_pila(pila):
     print("Estado actual de la pila:", pila)
+#Muestra todos los elementos de la lista#
 
 
 # --- Uso paso a paso ---
@@ -36,15 +41,20 @@ print("¿La pila está vacía?", esta_vacia(pila))
 apilar(pila, "Plato 1")
 apilar(pila, "Plato 2")
 apilar(pila, "Plato 3")
+#Agrega 3 elementos a la lista#
 
 mostrar_pila(pila)
 ver_tope(pila)
+#Muestra todo los elementos y el ultimo elemento de la lista#
 
 desapilar(pila)
 mostrar_pila(pila)
+#Elimina el ultimo elemento y muestra la lista#
 
 desapilar(pila)
 desapilar(pila)
 desapilar(pila)  # Intento extra
+#Intenta eliminar 3 veces el ultimo elemento de la lista, el último intento marcara que la lista esta vacia#
 
 print("¿La pila está vacía?", esta_vacia(pila))
+#Verifica que la lista este vacia, devuelve True#
